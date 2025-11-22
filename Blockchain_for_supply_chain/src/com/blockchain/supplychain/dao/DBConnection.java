9a761f7c4ba274bc5b2a88cdee7debc36359c2ea
SJAkash07 <suresh.24scse1420027@galgotiasuniversity.ac.in>
2025-11-22 19:34:12 +0530

    Update DBConnection.java

--- Blockchain_for_supply_chain/src/com/blockchain/supplychain/dao/DBConnection.java
+++ Blockchain_for_supply_chain/src/com/blockchain/supplychain/dao/DBConnection.java
@@ -8,9 +8,10 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/blockchain_supplychain";
     private static final String USER = "root";        
-    private static final String PASSWORD = "sja@mysql"; 
+    private static final String PASSWORD = "your_password"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
