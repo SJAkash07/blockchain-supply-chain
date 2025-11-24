94338f2e599b5c10004705e2d597ed9970a750f4
SJAkash07 <suresh.24scse1420027@galgotiasuniversity.ac.in>
2025-11-24 23:44:30 +0530

    Update GenerateHash.java

--- Blockchain_for_supply_chain/src/com/blockchain/supplychain/GenerateHash.java
+++ Blockchain_for_supply_chain/src/com/blockchain/supplychain/GenerateHash.java
@@ -4,8 +4,8 @@ import com.blockchain.supplychain.util.HashUtil;
 
 public class GenerateHash {
     public static void main(String[] args) {
-    // change "123456" if you want a different password
         String hash = HashUtil.applySha256("123456");
         System.out.println(hash);
     }
 }
+
